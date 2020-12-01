bf86e68326975461c67b0bbfdb2672a4f5b8b679
Tech-Recruiter-Justin <justin7623@gmail.com>
2020-12-01 23:44:36 +0800

    Added Todo

---
+++ Software_Development/Todo_List/app.js
@@ -0,0 +1,40 @@
+function todolist() {
+    const todo = [];
+    let newItem = null;   
+    let removeIndex = null; 
+    while (action !== "quit") {
+        var action = prompt("What would you like to do?");
+            if (action.toLowerCase() === "new"){
+                while (newItem === null || newItem.trim() === "" || newItem.trim() === " ") {
+                    newItem = prompt("What todo item do you want to add?");
+                }
+                todo.push(newItem.trim());
+                console.log(`${newItem.trim()} is added to the list`);
+                console.log("**************")
+                for (let item of todo) {
+                    console.log(`${todo.indexOf(item)}: ${item}`);
+                }
+                console.log("**************")
+                newItem = null;
+            } else if (action === "list") {
+                console.log("**************")
+                for (let item of todo) {
+                    console.log(`${todo.indexOf(item)}: ${item}`);
+                }
+                console.log("**************")
+            } else if  (action === "delete") {
+                removeIndex = prompt("Which item to remove? (input number)")
+                console.log(`${todo[removeIndex]} removed`);
+                todo.splice(removeIndex,1);
+                console.log("**************")
+                for (let item of todo) {
+                    console.log(`${todo.indexOf(item)}: ${item}`);
+                }
+                console.log("**************")
+            } else if (action === "quit") {
+                console.log("QUIT");
+            }
+    }
+}
+
+
